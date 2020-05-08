304ad4c7d434aad7f48851def606a21d3d5a62f1
William Broussard <wbroussard@me.com>
2020-05-08 16:25:41 -0500

    messages refined, more comments added.

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,7 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var characters = ["!", "@", "#", "$", "%", "^", "&", "*"]
-//"confirmed" variable refers to all used arrays selected in confirms.
+//"confirmed" variable refers to all used arrays selected in confirms. '[]' insures the confirmed variables are added to an array.
 //"length" variable refers to the length of the password decided in the prompt.
 var confirmed = [];
 var length;
@@ -13,6 +13,7 @@ var length;
 function writePassword() {
     //"do" and "while" serves to run the "if" confirms, while certain conditons defined after "while" are met.
     //"do" and "while" are essentially another form of "if" and "else".
+    //Study do/while loops at https://www.w3schools.com/jsref/jsref_dowhile.asp
     do {
         var useLowerCase = confirm("Do you want your password to include lower case letters?")
         var useUpperCase = confirm("Do you want your password to include upper case letters?")
@@ -24,9 +25,10 @@ function writePassword() {
     }
     }
     while (useLowerCase == false && useUpperCase == false && useNumbers == false && useCharacters == false)
-//Above "while" tells the prompts to appear if none of the options have been selected "yet", triggering the confirms.
+//Above "while" loop tells the prompts to appear if none of the options have been selected "yet", triggering the confirms.
+//See See 03-JavaScript Activity 32 for while loops.
     do {
-        length = prompt("How long do you want your password to be? *Must be between 8-128*")
+        length = prompt("How long do you want your password to be? *Must be between 8-128 characters.*")
     }
 //line below confirms that the input for length must be between 8 and 128.
     while (length < 8 || length > 128 || isNaN(length));
@@ -66,7 +68,8 @@ function generatePassword() {
 
     var password = "";
     for (var index = 0; index < parseInt(length); index++) {
-//for loop used for randomization of password  using the confirmed length and character type from the prompt and confirms.
+//for loop used for randomization of password using the confirmed length and character type from the prompt and confirms.
+//See 03-JavaScript Activity 21
         var character = Math.floor(Math.random() * confirmed.length);
         password += confirmed[character];
 
@@ -74,5 +77,5 @@ function generatePassword() {
 //returns the finished password
     return password;
 }
-// Adds an event listener to recognize the clicking of the generate password button.
+// Adds an event listener to recognize the clicking of the generate password button in index.html.
 generateBtn.addEventListener("click", writePassword);
